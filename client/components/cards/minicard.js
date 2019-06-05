ffc9fd0ee01af4229cd3f33d0294828e0742fd2e
lperez-finneg <lperez@finnegans.com.ar>
2019-06-05 18:34:04 -0300

    Botones de archivar y finalizar en las minicards

--- client/components/cards/minicard.js
+++ client/components/cards/minicard.js
@@ -14,7 +14,31 @@ BlazeComponent.extendComponent({
           Utils.goCardId(this.data().linkedId);
         else if (this.data().isLinkedBoard())
           Utils.goBoardId(this.data().linkedId);
-      }
+      },
+      'click .js-archive-card': Popup.afterConfirm('cardArchived', (action) => {
+        Popup.close();
+        const card = this.currentData().dataContext;
+        card.archive();
+      }),
+      'click .js-card-finished' : Popup.afterConfirm('cardFinished', (action) => {
+        Popup.close();
+        const card = this.currentData().dataContext;
+        if (card.isFinished()) {
+          Cards.update(card._id, {
+            $set: {
+              status : null,
+              endAt : null
+            }
+          });
+        } else {
+          Cards.update(card._id, {
+            $set: {
+              status : 'finished',
+              endAt : new Date()
+            }
+          });
+        }
+      }),
     }];
   },
 
